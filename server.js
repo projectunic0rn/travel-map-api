da45a2a206f75d1dfd253ab70edc97941c6e79c0
Matthew Rich <mrich@Matts-Macbook-Pro.local>
2019-06-05 16:16:38 -0500

    work

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const AuthService = require('./services/auth.service');
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 3000;
 
 
 const server = new ApolloServer({
