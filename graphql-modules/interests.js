2b9ecd1268dedff98f92e3f1ae56004a0b21efad
GreenWithMV <46555800+GreenWithMV@users.noreply.github.com>
2019-09-29 22:07:51 -0700

    Update interests.js

--- graphql-modules/interests.js
+++ graphql-modules/interests.js
@@ -1,10 +1,12 @@
 const { gql } = require('apollo-server');
 const User = require('../models').User;
+const Interest = require("../models").Interest;
 
 const typeDefs = gql`
    
     extend type Mutation {
         addInterest(InterestId: Int!): UserInterests!
+        addInterestType(name: String!): Interest!
     }
 
     type Interest {
@@ -35,8 +37,16 @@ const resolvers = {
                 console.log(err)
                 throw (err)
             }
-
-        }
+        },
+        addInterestType: async (_, { name }) => {
+            try {
+              let request = await Interest.create({"name": name});
+              return request;
+            } catch (err) {
+              console.log(err);
+              throw err;
+            }
+      }
     }
 
 }
