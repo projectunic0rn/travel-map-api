d2e653fc8214b2b171bc60a67fb0f25096848232
Matt Vance <mvance43776@gmail.com>
2019-10-06 01:17:59 -0700

    removing comment

--- migrations/20190515001421-create-user-interests.js
+++ migrations/20190515001421-create-user-interests.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('user_interests', {
+    return queryInterface.createTable("user_interests", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -12,10 +12,10 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false
       },
-//       name: {
-//         type: Sequelize.STRING,
-//         allowNull: false
-//       },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -24,9 +24,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    })
+    });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('user_interests');
+    return queryInterface.dropTable("user_interests");
   }
 };
