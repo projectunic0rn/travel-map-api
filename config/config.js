93b232f9165cd89289e35fc7c395732d1c44b6bc
Matt Vance <mvance43776@gmail.com>
2019-11-05 22:58:48 -0800

    Adding ability to delete all cities in a country at once

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME_DEV,
-    "password": process.env.DB_PASSWORD_DEV,
-    "database": process.env.DB_DATABASE_DEV,
-    "host": process.env.DB_HOST_DEV,
-    "dialect": "mysql"
+  development: {
+    username: "postgres",
+    password: "XSTAR1232",
+    database: "Geornal",
+    host: "localhost",
+    dialect: "postgres"
   },
-  "test": {
-    "username": process.env.DB_USERNAME_TEST,
-    "password": process.env.DB_PASSWORD_TEST,
-    "database": process.env.DB_DATABASE_TEST,
-    "host": process.env.DB_HOST_TEST,
-    "dialect": "mysql"
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_DATABASE_TEST,
+    host: process.env.DB_HOST_TEST,
+    dialect: "mysql"
   },
-  "production": {
-    "username": process.env.DB_USERNAME_PROD,
-    "password": process.env.DB_PASSWORD_PROD,
-    "database": process.env.DB_DATABASE_PROD,
-    "host": process.env.DB_HOST_PROD,
-    "dialect": "mysql"
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: "mysql"
   }
-}
+};
