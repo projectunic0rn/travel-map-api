72e012770ed6bdd7bb10d2784e14a64ad1b56be7
GreenWithMV <46555800+GreenWithMV@users.noreply.github.com>
2019-11-05 23:00:11 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "postgres",
-    password: "XSTAR1232",
-    database: "Geornal",
-    host: "localhost",
-    dialect: "postgres"
+    "username": process.env.DB_USERNAME_DEV,
+    "password": process.env.DB_PASSWORD_DEV,
+    "database": process.env.DB_DATABASE_DEV,
+    "host": process.env.DB_HOST_DEV,
+    "dialect": "mysql"
   },
   test: {
     username: process.env.DB_USERNAME_TEST,
