586813bbd3874b95d1384601fd7124c2f5cf27fb
Matt Vance <mvance43776@gmail.com>
2019-12-07 00:54:09 -0800

    Pointing to heroku database

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME_DEV,
-    password: process.env.DB_PASSWORD_DEV,
-    database: process.env.DB_DATABASE_DEV,
-    host: process.env.DB_HOST_DEV,
-    dialect: "mysql"
+    username: "qaufcmuvvrzunc",
+    password:
+      "22ca2cb5e864a6e9f3da9cd33b34f79a430223425f3a44dcec96fb3da780f520",
+    database: "deb2u8aa2ep3cj",
+    host: "ec2-23-21-160-38.compute-1.amazonaws.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   },
   test: {
     username: process.env.DB_USERNAME_TEST,
