2aca88b0d4c7a1305f33a8e560c67cf285adcbac
GreenWithMV <46555800+GreenWithMV@users.noreply.github.com>
2019-12-07 01:11:29 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -12,17 +12,25 @@ module.exports = {
     }
   },
   test: {
-    username: process.env.DB_USERNAME_TEST,
-    password: process.env.DB_PASSWORD_TEST,
-    database: process.env.DB_DATABASE_TEST,
-    host: process.env.DB_HOST_TEST,
-    dialect: "mysql"
+    username: "qaufcmuvvrzunc",
+    password:
+      "22ca2cb5e864a6e9f3da9cd33b34f79a430223425f3a44dcec96fb3da780f520",
+    database: "deb2u8aa2ep3cj",
+    host: "ec2-23-21-160-38.compute-1.amazonaws.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   },
   production: {
-    username: process.env.DB_USERNAME_PROD,
-    password: process.env.DB_PASSWORD_PROD,
-    database: process.env.DB_DATABASE_PROD,
-    host: process.env.DB_HOST_PROD,
-    dialect: "mysql"
+    username: "qaufcmuvvrzunc",
+    password:
+      "22ca2cb5e864a6e9f3da9cd33b34f79a430223425f3a44dcec96fb3da780f520",
+    database: "deb2u8aa2ep3cj",
+    host: "ec2-23-21-160-38.compute-1.amazonaws.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   }
 };
