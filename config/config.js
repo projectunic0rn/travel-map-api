1197187d383ae58c61f9642e41e505f410faa32c
Matt Vance <mvance43776@gmail.com>
2019-12-07 00:48:12 -0800

    Trying to push

--- config/config.js
+++ config/config.js
@@ -1,15 +1,11 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "qaufcmuvvrzunc",
-    password:
-      "22ca2cb5e864a6e9f3da9cd33b34f79a430223425f3a44dcec96fb3da780f520",
-    database: "deb2u8aa2ep3cj",
-    host: "ec2-23-21-160-38.compute-1.amazonaws.com",
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: true
-    }
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_USERNAME_DEV,
+    database: process.env.DB_USERNAME_DEV,
+    host: process.env.DB_USERNAME_DEV,
+    dialect: "mysql"
   },
   test: {
     username: process.env.DB_USERNAME_TEST,
