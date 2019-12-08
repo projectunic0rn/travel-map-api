878b5f213400084aeb2d8bba3c1846f938e9fff6
Matt Vance <mvance43776@gmail.com>
2019-12-08 01:32:34 -0800

    Profile-cities

--- migrations/create-city-reviews.js
+++ migrations/create-city-reviews.js
@@ -33,7 +33,7 @@ module.exports = {
         allowNull: false
       },
       comment: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: true
       },
       cost: {
