b64b85f4713029dc166ac2bb056b2e22d535ebcc
Tim Walsh <tim@mims.ms>
2020-05-06 17:41:57 -0700

    Adds migration to create friend_requests in schema
    Note: Will requiring the following commands to ensure all migrations are run as expected:
    
    ```
    npx sequelize-cli db:migrate:undo:all --to 20200303000000-create-city-reviews.js
    npx sequelize-cli db:migrate
    ```
    
    This will ensure the city-reviews migration is run under the expected file naming as well as ensure all migrations have timestamps (to retain explicit ordering of changes).

---
+++ migrations/20200506172355-create-friend-requests.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("friend_requests", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      senderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      receiverId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("friend_requests");
+  }
+};
