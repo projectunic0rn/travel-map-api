09c80e1d1269bee280adaa882aa8cd305acc4769
Matt Vance <mvance43776@gmail.com>
2020-09-16 23:28:03 -0700

    check

--- config/config.js
+++ config/config.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "traveler",
-    password: "mapquest",
-    database: "travelmaps_api_dev",
-    host: "localhost",
-    port: "5432",
-    dialect: "postgres"
+    username: "qaufcmuvvrzunc",
+    password:
+      "22ca2cb5e864a6e9f3da9cd33b34f79a430223425f3a44dcec96fb3da780f520",
+    database: "deb2u8aa2ep3cj",
+    host: "ec2-23-21-160-38.compute-1.amazonaws.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   },
   test: {
     username: "traveler_test",
